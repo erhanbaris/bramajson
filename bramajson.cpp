7296c2232d51418f76cc493b6d63b6ec29e87586
Erhan BARIS <erhanbaris@gmail.com>
2020-07-10 21:37:58 +0200

    Tests fixed

--- bramajson.cpp
+++ bramajson.cpp
@@ -39,39 +39,39 @@ void BRAMAJSON_ARRAY_SIZE_ASSERT(bramajson_object* output, size_t array_size) {
 
 char* ReadFile(char *filename)
 {
-   char *buffer = NULL;
-   int string_size, read_size;
-   FILE *handler = fopen(filename, "r");
+    char *buffer = NULL;
+    int string_size, read_size;
+    FILE *handler = fopen(filename, "r");
 
-   if (handler)
-   {
-       // Seek the last byte of the file
-       fseek(handler, 0, SEEK_END);
-       // Offset from the first to the last byte, or in other words, filesize
-       string_size = ftell(handler);
-       // go back to the start of the file
-       rewind(handler);
+    if (handler)
+    {
+        // Seek the last byte of the file
+        fseek(handler, 0, SEEK_END);
+        // Offset from the first to the last byte, or in other words, filesize
+        string_size = ftell(handler);
+        // go back to the start of the file
+        rewind(handler);
 
-       // Allocate a string that can hold it all
-       buffer = (char*) malloc(sizeof(char) * (string_size + 1) );
+        // Allocate a string that can hold it all
+        buffer = (char*) malloc(sizeof(char) * (string_size + 1) );
 
-       // Read it all in one operation
-       read_size = fread(buffer, sizeof(char), string_size, handler);
+        // Read it all in one operation
+        read_size = fread(buffer, sizeof(char), string_size, handler);
 
-       // fread doesn't set it so put a \0 in the last position
-       // and buffer is now officially a string
-       buffer[string_size] = '\0';
+        // fread doesn't set it so put a \0 in the last position
+        // and buffer is now officially a string
+        buffer[string_size] = '\0';
 
-       if (string_size != read_size)
-       {
-           // Something went wrong, throw away the memory and set
-           // the buffer to NULL
-           free(buffer);
-           buffer = NULL;
-       }
+        if (string_size != read_size)
+        {
+            // Something went wrong, throw away the memory and set
+            // the buffer to NULL
+            free(buffer);
+            buffer = NULL;
+        }
 
-       // Always remember to close the file.
-       fclose(handler);
+        // Always remember to close the file.
+        fclose(handler);
     }
 
     return buffer;
@@ -79,51 +79,46 @@ char* ReadFile(char *filename)
 
 int main()
 {
-        char* content = ReadFile("citylots.json");
-        BRAMAJSON_VALIDATE_ASSERT(content, BRAMAJSON_SUCCESS);
 
-        bramajson_object* output = NULL;
-        BRAMAJSON_VALIDATE_ASSERT("{'erhan': 'test', 'baris': 'test'}", BRAMAJSON_SUCCESS);
+    bramajson_object* output = NULL;
+    BRAMAJSON_VALIDATE_ASSERT("{'erhan': 'test', 'baris': 'test'}", BRAMAJSON_SUCCESS);
 
-        output = BRAMAJSON_ARRAY_ASSERT("[  true   ,false \r\n,\r\n1,1.2]", BRAMAJSON_SUCCESS, 4);
-        assert(output->_array->head->type == BRAMAJSON_TRUE);
-        assert(output->_array->head->type == BRAMAJSON_FALSE);
-        assert(output->_array->head->next->type == BRAMAJSON_INT);
-        assert(output->_array->head->next->_integer == 1);
-        assert(output->_array->head->next->_double  ==  1.2);
+    output = BRAMAJSON_ARRAY_ASSERT("[  true   ,false \r\n,\r\n1,1.2]", BRAMAJSON_SUCCESS, 4);
+    assert(output->_array->head->type == BRAMAJSON_TRUE);
+    assert(output->_array->head->next->type == BRAMAJSON_FALSE);
+    assert(output->_array->head->next->next->type == BRAMAJSON_INT);
+    assert(output->_array->head->next->next->_integer == 1);
+    assert(output->_array->head->next->next->next->_double  ==  1.2);
 
-        BRAMAJSON_VALIDATE_ASSERT("{}", BRAMAJSON_SUCCESS);
-        BRAMAJSON_VALIDATE_ASSERT("{'erhan': 'test'}", BRAMAJSON_SUCCESS);
-        BRAMAJSON_VALIDATE_ASSERT("[1024]", BRAMAJSON_SUCCESS);
-        BRAMAJSON_ARRAY_ASSERT("['erhan', [], [true], ['']]", BRAMAJSON_SUCCESS, 4);
-        BRAMAJSON_ARRAY_ASSERT("['erhan', \"baris\", false]", BRAMAJSON_SUCCESS, 3);
-        BRAMAJSON_ARRAY_ASSERT("[]", BRAMAJSON_SUCCESS, 0);
-        BRAMAJSON_ARRAY_ASSERT("[true]", BRAMAJSON_SUCCESS, 1);
-        BRAMAJSON_ARRAY_ASSERT("[true, false]", BRAMAJSON_SUCCESS, 2);
-        output = BRAMAJSON_ARRAY_ASSERT("[[true, false]]", BRAMAJSON_SUCCESS, 1);
-        assert(output->_array->head->type == BRAMAJSON_ARRAY);
-        BRAMAJSON_ARRAY_SIZE_ASSERT(output->_array->head, 2);
-        
-        output = BRAMAJSON_ARRAY_ASSERT("[[[true, false]]]", BRAMAJSON_SUCCESS, 1);
-        assert(output->_array->head->type == BRAMAJSON_ARRAY);
-        BRAMAJSON_ARRAY_SIZE_ASSERT(output->_array->head, 1);
-        assert(output->_array->head->_array->head->type == BRAMAJSON_ARRAY);
-        BRAMAJSON_ARRAY_SIZE_ASSERT(output->_array->head->_array->head, 2);
+    BRAMAJSON_VALIDATE_ASSERT("{}", BRAMAJSON_SUCCESS);
+    BRAMAJSON_VALIDATE_ASSERT("{'erhan': 'test'}", BRAMAJSON_SUCCESS);
+    BRAMAJSON_VALIDATE_ASSERT("[1024]", BRAMAJSON_SUCCESS);
+    BRAMAJSON_ARRAY_ASSERT("['erhan', [], [true], ['']]", BRAMAJSON_SUCCESS, 4);
+    BRAMAJSON_ARRAY_ASSERT("['erhan', \"baris\", false]", BRAMAJSON_SUCCESS, 3);
+    BRAMAJSON_ARRAY_ASSERT("[]", BRAMAJSON_SUCCESS, 0);
+    BRAMAJSON_ARRAY_ASSERT("[true]", BRAMAJSON_SUCCESS, 1);
+    BRAMAJSON_ARRAY_ASSERT("[true, false]", BRAMAJSON_SUCCESS, 2);
+    output = BRAMAJSON_ARRAY_ASSERT("[[true, false]]", BRAMAJSON_SUCCESS, 1);
+    assert(output->_array->head->type == BRAMAJSON_ARRAY);
+    BRAMAJSON_ARRAY_SIZE_ASSERT(output->_array->head, 2);
 
-        BRAMAJSON_ARRAY_ASSERT("[true, false]", BRAMAJSON_SUCCESS, 2);
+    output = BRAMAJSON_ARRAY_ASSERT("[[[true, false]]]", BRAMAJSON_SUCCESS, 1);
+    assert(output->_array->head->type == BRAMAJSON_ARRAY);
+    BRAMAJSON_ARRAY_SIZE_ASSERT(output->_array->head, 1);
+    assert(output->_array->head->_array->head->type == BRAMAJSON_ARRAY);
+    BRAMAJSON_ARRAY_SIZE_ASSERT(output->_array->head->_array->head, 2);
 
-        BRAMAJSON_VALIDATE_ASSERT("[true, ,false]", BRAMAJSON_JSON_NOT_VALID);
-        BRAMAJSON_VALIDATE_ASSERT("[,]", BRAMAJSON_JSON_NOT_VALID);
-        BRAMAJSON_VALIDATE_ASSERT("[,'erhan']", BRAMAJSON_JSON_NOT_VALID);
-        BRAMAJSON_VALIDATE_ASSERT("['erhan',]", BRAMAJSON_JSON_NOT_VALID);
-        
-        BRAMAJSON_VALIDATE_ASSERT("1024.1", BRAMAJSON_SUCCESS);
-        BRAMAJSON_VALIDATE_ASSERT("1024.1", BRAMAJSON_SUCCESS);
+    BRAMAJSON_ARRAY_ASSERT("[true, false]", BRAMAJSON_SUCCESS, 2);
 
+    BRAMAJSON_VALIDATE_ASSERT("[true, ,false]", BRAMAJSON_JSON_NOT_VALID);
+    BRAMAJSON_VALIDATE_ASSERT("[,]", BRAMAJSON_JSON_NOT_VALID);
+    BRAMAJSON_VALIDATE_ASSERT("[,'erhan']", BRAMAJSON_JSON_NOT_VALID);
+    BRAMAJSON_VALIDATE_ASSERT("['erhan',]", BRAMAJSON_JSON_NOT_VALID);
 
-        BRAMAJSON_VALIDATE_ASSERT("          ", BRAMAJSON_JSON_NOT_VALID);
+    BRAMAJSON_VALIDATE_ASSERT("1024.1", BRAMAJSON_SUCCESS);
+    BRAMAJSON_VALIDATE_ASSERT("1024.1", BRAMAJSON_SUCCESS);
 
-        BRAMAJSON_VALIDATE_ASSERT("{\"menu\": {\n"
+    BRAMAJSON_VALIDATE_ASSERT("{\"menu\": {\n"
                               "  \"id\": \"file\",\n"
                               "  \"value\": \"File\",\n"
                               "  \"popup\": {\n"
@@ -189,5 +184,5 @@ int main()
     BRAMAJSON_VALIDATE_ASSERT("false", BRAMAJSON_SUCCESS);
     BRAMAJSON_VALIDATE_ASSERT("null", BRAMAJSON_SUCCESS);
     BRAMAJSON_ARRAY_ASSERT("[[[[[[[[[[[[1],1],1],1],1],1],1],1],1],1],1],1]", BRAMAJSON_SUCCESS, 2);
-	return 0;
+    return 0;
 }
